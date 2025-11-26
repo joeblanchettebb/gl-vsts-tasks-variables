f2ec728d60a227e4fc4e19fd2389e2441f965f81
Joseph Blanchette <joseph.blanchette@blackbaud.me>
2025-11-26 11:09:31 -0500

    Upgrade node version

--- Tasks/LoadJsonVariables/loadjsonvariables.ts
+++ Tasks/LoadJsonVariables/loadjsonvariables.ts
@@ -1,6 +1,6 @@
 import fs = require("fs-extra");
 import tl = require("azure-pipelines-task-lib");
-import { recursiveProcessing } from "./common/expandJObject";
+import { recursiveProcessing } from "../../Common/Node/expandJObject";
 
 try {
     let source = tl.getPathInput("JsonSource");
